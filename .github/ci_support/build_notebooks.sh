b34b1d9e378b3db5ecea4477bbd0e37375103242
muh-hassani <muhammad@cmleo14.mpie.de>
2021-04-27 00:03:28 +0200

    updating build_notebooks.sh

--- .github/ci_support/build_notebooks.sh
+++ .github/ci_support/build_notebooks.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
-# pyiron config 
-python .github/ci_support/pyironconfig.py
-
-# import dataset
-bash .github/ci_support/import_dataset.sh
-
-# install nglview
-bash .github/ci_support/install_nglview.sh
-
 # conda install papermill
 conda install -c conda-forge papermill
 
 # execute notebooks
 current_dir=$(pwd)
 i=0;
-for f in $(find . -name "*.ipynb" | sort -n); do
-    cd $(dirname $f);
-    notebook=$(basename $f);
-    papermill ${notebook} ${notebook%.*}-out.${notebook##*.} -k "python3" || i=$((i+1));
-    cd $current_dir;
+for dir in $current_dir/*/ ; do 
+    for f in $(find $dir -name *.ipynb | sort -n); do
+        cd $dir;
+        notebook=$(basename $f);
+        papermill ${notebook} ${notebook%.*}-out.${notebook##*.} -k "python3" || i=$((i+1));
+        cd $current_dir;
+    done;
 done;
 
 # push error to next level
